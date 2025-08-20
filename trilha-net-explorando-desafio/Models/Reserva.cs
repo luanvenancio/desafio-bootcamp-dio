1bb51fbdcdb853a6c014bc2a4ca75390f1d121da
luanvenanciosoitic <luan.venancio04@hotmail.com>
2025-08-20 00:13:16 -0300

    add project

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,50 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count() <= Suite.Capacidade())
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A quantidade de hóspedes excede a capacidade da suíte.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count();
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m;
+            }
+
+            return valor;
+        }
+    }
+}
